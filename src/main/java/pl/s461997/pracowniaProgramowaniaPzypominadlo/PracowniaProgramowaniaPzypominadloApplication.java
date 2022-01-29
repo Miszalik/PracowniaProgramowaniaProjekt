7ddce84e73e72ca74d8394d02f922e79f6a4c2ae
Miszalik <mateusz.miszalik@wp.pl>
2022-01-29 17:35:07 +0100

    Pierwsze, testowe uruchomienie projektu

---
+++ src/main/java/pl/s461997/pracowniaProgramowaniaPzypominadlo/PracowniaProgramowaniaPzypominadloApplication.java
@@ -0,0 +1,13 @@
+package pl.s461997.pracowniaProgramowaniaPzypominadlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracowniaProgramowaniaPzypominadloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracowniaProgramowaniaPzypominadloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/s461997/pracowniaProgramowaniaPzypominadlo/PracowniaProgramowaniaPzypominadloApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.s461997.pracowniaProgramowaniaPzypominadlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracowniaProgramowaniaPzypominadloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
