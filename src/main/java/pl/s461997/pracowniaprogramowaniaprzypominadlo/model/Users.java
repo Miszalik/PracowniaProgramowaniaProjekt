cae26af65f2ce537d6630d29d25c585f67daef4a
Miszalik <mateusz.miszalik@wp.pl>
2022-01-30 23:33:04 +0100

    Przygotowanie testow do projetu i przygotowanie pelaczenia z baza danych

--- src/main/java/pl/s461997/pracowniaprogramowaniaprzypominadlo/model/Users.java
+++ src/main/java/pl/s461997/pracowniaprogramowaniaprzypominadlo/model/Users.java
@@ -14,7 +14,7 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+//@AllArgsConstructor
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO) //???
--- src/test/java/pl/s461997/pracowniaprogramowaniaprzypominadlo/controller/UsersControllerTest.java
+++ src/test/java/pl/s461997/pracowniaprogramowaniaprzypominadlo/controller/UsersControllerTest.java
@@ -1,12 +1,18 @@
 package pl.s461997.pracowniaprogramowaniaprzypominadlo.controller;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import pl.s461997.pracowniaprogramowaniaprzypominadlo.model.Users;
 
 import javax.annotation.PostConstruct;
 
+import java.util.HashSet;
+
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,11 +25,14 @@ class UsersControllerTest {
 
     @PostConstruct
     public void init() {
-        uri = "http://localhost/:" + port;
+        uri = "http://localhost:" + port;
     }
 
     @Test
     void findAllUsers() {
+        RestAssured.get(uri + "/users/")
+                .then()
+                .statusCode(200);
     }
 
     @Test
@@ -32,10 +41,39 @@ class UsersControllerTest {
 
     @Test
     void addUser() {
+        Users user = new Users();
+        user.setUsername("Sebek");
+        user.setListname(new HashSet<>());
+
+        given()
+                .when()
+                .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                .body(user)
+                .post(uri + "/users/")
+                .then()
+                .statusCode(500);
+
+        Users newUser = given()
+                .when()
+                .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                .body(user)
+                .post(uri + "/users/")
+                .then()
+                .statusCode(201)
+                .extract().body().as(Users.class);
+
+        assertNotNull(newUser);
+        assertNotNull(newUser.getId());
     }
 
     @Test
     void deleteUser() {
+        given()
+                .pathParam("id", 300)
+                .delete(uri + "/users/{id}")
+                .prettyPeek()
+                .then()
+                .statusCode(404);
     }
 
     @Test
