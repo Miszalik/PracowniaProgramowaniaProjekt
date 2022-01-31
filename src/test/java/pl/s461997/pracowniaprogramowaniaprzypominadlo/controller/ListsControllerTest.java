6daaba734d357cecdeed891d922d03c33ab510ad
Miszalik <mateusz.miszalik@wp.pl>
2022-01-31 02:17:31 +0100

    Poprawa i rozwiniecie testow na pozostale klasy

--- src/test/java/pl/s461997/pracowniaprogramowaniaprzypominadlo/controller/ListsControllerTest.java
+++ src/test/java/pl/s461997/pracowniaprogramowaniaprzypominadlo/controller/ListsControllerTest.java
@@ -1,17 +1,36 @@
 package pl.s461997.pracowniaprogramowaniaprzypominadlo.controller;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import pl.s461997.pracowniaprogramowaniaprzypominadlo.model.Lists;
 
+import javax.annotation.PostConstruct;
+
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ListsControllerTest {
+    @LocalServerPort
+    private int port;
+
+    private String uri;
+
+    @PostConstruct
+    public void init() {
+        uri = "http://localhost:" + port;
+    }
 
     @Test
     void findAllLists() {
+        RestAssured.get(uri + "/lists/")
+                .then()
+                .statusCode(200);
     }
 
     @Test
@@ -20,10 +39,30 @@ class ListsControllerTest {
 
     @Test
     void addList() {
+        Lists list = new Lists();
+        list.setListname("Tostowe zadanie");
+
+        Lists newList = given()
+                .when()
+                .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                .body(list)
+                .post(uri + "/lists/")
+                .then()
+                .statusCode(201)
+                .extract().body().as(Lists.class);
+
+        assertNotNull(newList);
+        assertNotNull(newList.getId());
     }
 
     @Test
     void deleteList() {
+        given()
+                .pathParam("id", 30000)
+                .delete(uri + "/lists/{id}")
+                .prettyPeek()
+                .then()
+                .statusCode(404);
     }
 
     @Test
--- src/test/java/pl/s461997/pracowniaprogramowaniaprzypominadlo/controller/TasksControllerTest.java
+++ src/test/java/pl/s461997/pracowniaprogramowaniaprzypominadlo/controller/TasksControllerTest.java
@@ -1,17 +1,36 @@
 package pl.s461997.pracowniaprogramowaniaprzypominadlo.controller;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import pl.s461997.pracowniaprogramowaniaprzypominadlo.model.Tasks;
 
+import javax.annotation.PostConstruct;
+
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class TasksControllerTest {
+    @LocalServerPort
+    private int port;
+
+    private String uri;
+
+    @PostConstruct
+    public void init() {
+        uri = "http://localhost:" + port;
+    }
 
     @Test
-    void findAllTasks() {
+    void findAllTests() {
+        RestAssured.get(uri + "/tasks/")
+                .then()
+                .statusCode(200);
     }
 
     @Test
@@ -20,10 +39,31 @@ class TasksControllerTest {
 
     @Test
     void addTask() {
+        Tasks task = new Tasks();
+        task.setTaskname("Tostowe zadanie");
+        task.setDescription("Testowy opis");
+
+        Tasks newTask = given()
+                .when()
+                .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                .body(task)
+                .post(uri + "/tasks/")
+                .then()
+                .statusCode(201)
+                .extract().body().as(Tasks.class);
+
+        assertNotNull(newTask);
+        assertNotNull(newTask.getId());
     }
 
     @Test
     void deleteTask() {
+        given()
+                .pathParam("id", 30000)
+                .delete(uri + "/tasks/{id}")
+                .prettyPeek()
+                .then()
+                .statusCode(404);
     }
 
     @Test
